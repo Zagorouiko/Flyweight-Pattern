f386c2c67be60d8a757badd85900c6fe1de8cd04
Zagorouiko <Zagorouiko@gmail.com>
2020-04-01 16:17:15 -0700

    set up ecs example

---
+++ Assets/Scripts/SpawnCar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCar : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject camera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 position = new Vector3(10, 10, 10);
+            GameObject carInstance = Instantiate(car, position, Quaternion.identity);
+            camera.GetComponent<SmoothFollow>().target = carInstance.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject cube;
+    public int rows;
+    public int columns;
+
+    void Start()
+    {
+        for (int x = 0; x < rows; x++)
+        {
+            for (int z = 0; z < columns; z++)
+            {
+                GameObject instance = Instantiate(cube);
+                Vector3 position = new Vector3(x, 
+                    Mathf.PerlinNoise(x * 0.21f, z * 0.21f), 
+                    z);
+
+                instance.transform.position = position;
+            }
+        }  
+    }
+
+ 
+}
